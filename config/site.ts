d09f2984264ddbfc39f0ca469702a300137e5854
brutalvik <vikcanada90@gmail.com>
2025-05-19 16:49:02 -0600

    Added .ico file and updated site configuration

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "V-Bytes",
+  description: "Experience the future of web development with V-Bytes.",
   navItems: [
     {
       label: "Home",
