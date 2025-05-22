2caef6ecb8cd4f334759779595696a3f73a9ef50
brutalvik <vikcanada90@gmail.com>
2025-05-22 03:35:24 -0600

    removed lazy loading of spline due to deployment issues

--- components/ui/robot.tsx
+++ components/ui/robot.tsx
@@ -1,17 +1,8 @@
 "use client";
 
-import dynamic from "next/dynamic";
+import Spline from "@splinetool/react-spline";
 import { CDN } from "@lib/config";
 
-
-// Lazy load Spline with SSR disabled and a fallback loader scene
-const LazySpline = dynamic<{ scene: string }>(
-  () => import("@splinetool/react-spline").then((mod) => mod.default),
-  {
-    ssr: false,
-  }
-);
-
 export default function Robot() {
-  return <LazySpline scene={CDN.robotUrl as string} />;
+  return <Spline scene={CDN.robotUrl as string} />;
 }
