fae1d9b50e9e197180375ca0d07c5a8f71258ed7
brutalvik <vikcanada90@gmail.com>
2025-05-31 19:42:56 -0600

    debug 2

--- app/api/get-countries/route.ts
+++ app/api/get-countries/route.ts
@@ -4,10 +4,13 @@ import { v4 as uuidv4 } from "uuid";
 import { CDN } from "@/lib/config"; 
 
 export async function GET() {
+  console.log("vercel hit the api");
   try {
+    console.log("entered api");
     const response = await axios.get(
       "https://lpkeoq39v7.execute-api.us-east-2.amazonaws.com/countries?file=country_code.json"
     );
+    console.log("RESPONSE : ", response);
     const countries = response.data;
     const countriesWithIds = countries.map((country: any) => ({
       ...country,
@@ -16,6 +19,7 @@ export async function GET() {
 
     return NextResponse.json(countriesWithIds, { status: 200 });
   } catch (error: any) {
+    console.log("failed api");
     console.error("Error fetching country codes:", error?.response?.data || error.message);
     return NextResponse.json({ success: false, error: "Internal Server Error" }, { status: 500 });
   }
