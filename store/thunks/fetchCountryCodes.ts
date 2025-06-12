d11337a687a027d7a0e2413f7fb61f820501cdfc
Vikram Kumar <vikcanada90@gmail.com>
2025-06-12 15:21:54 -0600

    fixed countrycodes

--- store/thunks/fetchCountryCodes.ts
+++ store/thunks/fetchCountryCodes.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 
 export const fetchCountryCodes = createAsyncThunk("countryCodes/fetch", async (_, thunkAPI) => {
   try {
-    const res = await axios.get("/api/get-countries");
+    const res = await axios.get("/dataset/countrycodes.json");
     return res.data;
   } catch (err: any) {
-    return thunkAPI.rejectWithValue(err?.response?.data || "Failed to fetch countries");
+    return thunkAPI.rejectWithValue(err?.response?.data || "Failed to load country codes");
   }
 });
